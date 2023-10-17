33feb98126949e51ab5c5f82f828a42652089095
lloydttt <126597991+lloydttt@users.noreply.github.com>
2023-10-18 00:29:11 +0800

    in

--- Vo_Lighting/ec11.cpp
+++ Vo_Lighting/ec11.cpp
@@ -10,8 +10,22 @@ void EC11::ec11_init(){
 
 }
 
-int EC11::ec11_check(){
-    
-}
+int EC11::ec11_scan(){
+    int s_sta = digitalRead(check_key);
+    int a_sta = digitalRead(ec11_a);
+    int b_sta = digitalRead(ec11_b);
+    if(s_sta == 1){
+        silence = 1;
+        
+    }
+    else{
+        silence = 0;
 
+    }
+
+
+
+
+
+}
 
